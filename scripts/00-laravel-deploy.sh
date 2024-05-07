97288934b551c40154f5068b58bcbf51c74d6bf3
sergannn <su.vasiliev@gmail.com>
2024-05-07 15:49:52 +0300

    Update 00-laravel-deploy.sh
    update

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+
+echo "Updating Composer..."
+composer self-update --2
 echo "Running composer"
 composer global require hirak/prestissimo
 composer install --no-dev --working-dir=/var/www/html
