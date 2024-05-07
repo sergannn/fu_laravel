c65423360c4acfbbb7982d3965aeb731b8c88e91
sergannn <su.vasiliev@gmail.com>
2024-05-08 00:31:56 +0300

    Add existing project files to Git

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,12 @@
 <?php
+
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
@@ -14,3 +16,5 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+require __DIR__.'/auth.php';
