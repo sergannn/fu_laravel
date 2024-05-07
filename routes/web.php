f0f1db42afbf671d0264bc94cee11d9946ca2978
sergannn <su.vasiliev@gmail.com>
2024-05-08 01:16:21 +0300

    Update web.php
    ser rrr

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
+Route::get('/ser',function() { return ' ser'; });
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
