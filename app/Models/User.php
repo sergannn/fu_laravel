f9e7f0ee63b2cfbb815f5e765940fb91a42dca59
sergannn <su.vasiliev@gmail.com>
2024-05-12 00:57:59 +0300

    Update User.php
    dsfsd

--- app/Models/User.php
+++ app/Models/User.php
@@ -44,4 +44,9 @@ class User extends Authenticatable
             'password' => 'hashed',
         ];
     }
+    public function markers()
+    {
+    return $this->hasMany(Marker::class);
+    }
+    
 }
