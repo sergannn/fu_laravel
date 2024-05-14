8f0318cd37e44e4b58548f2ee3a58ef563c87119
sergannn <su.vasiliev@gmail.com>
2024-05-14 17:20:56 +0300

    Update api.php
    auth

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,16 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\Auth\AuthenticatedSessionController;
+use App\Http\Controllers\Auth\ConfirmablePasswordController;
+use App\Http\Controllers\Auth\EmailVerificationNotificationController;
+use App\Http\Controllers\Auth\EmailVerificationPromptController;
+use App\Http\Controllers\Auth\NewPasswordController;
+use App\Http\Controllers\Auth\PasswordController;
+use App\Http\Controllers\Auth\PasswordResetLinkController;
+use App\Http\Controllers\Auth\RegisteredUserController;
+use App\Http\Controllers\Auth\VerifyEmailController;
+
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
@@ -16,3 +26,9 @@ Route::post('/markers', [MarkerController::class, 'storeApi']);
 
 Route::post('/register', 'AuthController@register');
 Route::post('/login', 'AuthController@login');
+
+use App\Http\Controllers\UserAuthController;
+
+Route::get('login', [UserAuthController::class, 'login']);
+
+
