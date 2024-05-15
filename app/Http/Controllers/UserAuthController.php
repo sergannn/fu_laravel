cb2e18ac7ad6b7c95173ac7aac66c31f0f37bb50
sergannn <su.vasiliev@gmail.com>
2024-05-16 02:54:26 +0300

    Update UserAuthController.php
    log out

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -9,29 +9,44 @@ use App\Models\User;
 
 class UserAuthController extends Controller
 {
-    //
-public function login(Request $request)
-{
-    //echo 'hello';
-    $loginUserData = $request->validate([
-        'email' => 'required|string|email',
-        'password' => 'required|min:1'
-    ]);
+            //
+        public function login(Request $request)
+        {
+            //echo 'hello';
+            $loginUserData = $request->validate([
+                'email' => 'required|string|email',
+                'password' => 'required|min:1'
+            ]);
 
-    $user = User::where('email', $loginUserData['email'])->first();
+            $user = User::where('email', $loginUserData['email'])->first();
 
-    if (!$user ||!Hash::check($loginUserData['password'], $user->password)) {
-        return response()->json([
-            'message' => 'Invalid Credentials'
-        ], 401);
-    }
+            if (!$user ||!Hash::check($loginUserData['password'], $user->password)) {
+                return response()->json([
+                    'message' => 'Invalid Credentials'
+                ], 401);
+            }
 
-    $token = $user->createToken($user->name. '-AuthToken')->plainTextToken;
+            $token = $user->createToken($user->name. '-AuthToken')->plainTextToken;
 
-   
-    return response()->json([
-        'access_token' => $token,
-        'user_id' => $user->id, 
-    ]);
+            return response()->json([
+                'access_token' => $token,
+                'user_id' => $user->id, 
+            ]);
 }
+
+        public function logout(Request $request)
+        {
+        // Retrieve the current authenticated user
+        $user = Auth::user();
+
+        // Revoke the access token
+        $user->currentAccessToken()->delete();
+
+        // Optionally, clear the authentication guard to log out the user completely
+        Auth::guard('api')->logout();
+
+        return response()->json([
+        'message' => 'Logged out successfully'
+        ]);
+        }
 }
