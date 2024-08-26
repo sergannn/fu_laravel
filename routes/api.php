c0c4d4888de78547e388e0bcf82625caca3eda63
sergannn <su.vasiliev@gmail.com>
2024-08-26 17:02:41 +0300

    Update api.php
    online controller

--- routes/api.php
+++ routes/api.php
@@ -13,7 +13,9 @@ use App\Http\Controllers\Auth\PasswordResetLinkController;
 use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 
+use App\Http\Controllers\Auth\OnlineController;
 
+Route::get('/api/update-value', [OnlineController::class, 'updateValue']);
 Route::post('/email/verify/{id}/{bool}', [VerifyEmailController::class, '__invoke']);
 
 Route::get('/email/verify/{id}/{bool}', [VerifyEmailController::class, '__invoke']);
